0a58c834283e8b53f8a30dcd15fa61e77649cebe
LuisPADD <maryofgod1706@gmail.com>
2025-10-15 18:38:20 -0300

    Fim-ModuloJS

---
+++ JS/ASync/index.js
@@ -0,0 +1,27 @@
+function buscarVelocidadeAtual(){
+
+    return new Promise((resolve,reject)=>{
+        
+        //Acessar o equipamento de velocidade atual
+
+        setTimeout(() => {
+            result(110);
+        }, 6000);
+
+        setTimeout(() => {
+            console.log('acessando equipamento de calculo!');
+        }, 2000);
+
+        setTimeout(() => {
+            console.log('Calculando e processando informações obtidas!')
+        }, 4000);
+
+    });
+}
+const result =  buscarVelocidadeAtual();
+result.then((velocidade)=>{
+    console.log("A velocidade atual é de: "+velocidade+' KM/h')
+}).catch(()=>{
+    console.log("Ocorreu um erro ao ler a velocidade atual do veiculo!")
+});
+
