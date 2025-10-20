92c5cfd99129c84ef167195dbcb6ccc0d34806f0
LuisPADD <maryofgod1706@gmail.com>
2025-10-20 20:26:24 -0300

    ReactJS - map list

--- reactjs/src/App.tsx
+++ reactjs/src/App.tsx
@@ -1,65 +1,25 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [primeiroNumero, setPrimeiroNumero] = useState<number>(0);
-  const [operacao, setOperacao] = useState('');
-  const [segundoNumero, setsegundoNumero] = useState<number>(0);
-  const [resultado, setResultado] = useState<number>(0);
+  const usuarios = [
+    {name: 'Vitor', lastname: 'Manoel'},
+    {name: 'Ana', lastname: 'Nogueira'},
+  ];
 
-  useEffect(() => {
-
-    switch (operacao) {
-      case '+': 
-        let resultado = primeiroNumero + segundoNumero;
-
-        if(!isNaN(resultado)) {
-          setResultado(resultado);
-        };        
-
-        break;
-      case '-':
-          setResultado(primeiroNumero-segundoNumero);
-        break;
-      case '*':
-          setResultado(primeiroNumero*segundoNumero);
-        break;
-      case '/':
-          setResultado(primeiroNumero/segundoNumero);
-        break;
-      case '':
-          setResultado(0);
-        break;
-      default:
-
-        break;
-    }
-    
-  }, [primeiroNumero, operacao, segundoNumero]);
-
-  function handlesubmit(e: React.FormEvent){
-    e.preventDefault();
-
-    
-  }
-
-
-  return (  
+  return (
     <>
 
-      <input type="number" onChange={(e)=>{setPrimeiroNumero(e.target.valueAsNumber)}} value={primeiroNumero}/>
+      {usuarios.map((nomes) => {
+        return (
+          <>
+            {nomes.name} {nomes.lastname}
+             <br />
+          </>
+        )
+      }
+      )
+      }
 
-      <select onChange={(e)=>{setOperacao(e.target.value)}} value={operacao}>
-        <option value="">-- Selecione a operação --</option>
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="/">/</option>
-        <option value="*">*</option>
-      </select>
-
-      <input type="number" onChange={(e)=>{setsegundoNumero(e.target.valueAsNumber)}} value={segundoNumero}/>
-      
-      <br />
-      <b>RESULTADO: {resultado}</b>
     </>
   );
 }
