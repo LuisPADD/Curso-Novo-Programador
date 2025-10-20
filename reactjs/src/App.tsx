aa0893f3ee0130bcdd61748c4bcc3edc88859d92
LuisPADD <maryofgod1706@gmail.com>
2025-10-20 20:51:54 -0300

    ReactJS - Direct Style

--- reactjs/src/App.tsx
+++ reactjs/src/App.tsx
@@ -6,11 +6,14 @@ function App() {
 
   return (
     <>
-      {logged && <p>Promoçoes do Dia</p>}
-      
-      {!logged && <p>Você não está logado. Faça login para ver as promoçoes do dia</p>}
 
-      <button>Logar</button>
+      <h1 style={{color:'black', fontSize: '45px', backgroundColor: 'gray'}}>Titulo do BLOG</h1>
+      <h2 style={{color: 'blue', fontSize: '150px'}}>Subtitulo do BLOG</h2>
+
+      <br />  
+      
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut sunt tempore officia, accusantium nulla sed nam ipsam. Possimus expedita blanditiis, ea soluta sed ab ullam vitae, harum laborum quo dolorum.</p>
+
 
     </>
   );
